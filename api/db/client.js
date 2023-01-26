912b8177499f83ff973933a5fd6fc6a97e606521
adpears94 <pears.avery@gmail.com>
2023-01-25 20:47:45 -0500

    something

--- api/db/client.js
+++ api/db/client.js
@@ -1,6 +1,6 @@
 const { Client } = require("pg");
 const client = new Client({
-  host: process.env.PGHOST || "localhost",
+  host: process.env.PGHOST || "vehicle-gate-kiosk.ca4lcabxs5eo.us-gov-west-1.rds.amazonaws.com",
   user: process.env.PGUSER || 'postgres',
   port: process.env.PGPORT || 5432,
   password: process.env.PGPASSWORD || 'password',
