88cc334c79b93135fdd4147faea38b544e19ce6b
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2023-08-26 17:57:43 -0400

    pokebug search

--- ui/src/components/login/login.jsx
+++ ui/src/components/login/login.jsx
@@ -128,7 +128,9 @@ export const Login = () => {
           // console.log("after local storage is set", localStorage);
           setIsAuthenticated(data.user.admin);
           setFlag(true);
-          navigate("/forms");
+          // navigate("/forms");
+          console.log("data from login", data)
+          console.log("hitting this function")
         }
       })
       .catch((err) => console.log(err));
