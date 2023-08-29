73219dd094f9289674b73e1031b0b81cdb539f09
yurikgarcia <90262671+yurikgarcia@users.noreply.github.com>
2023-08-29 16:28:44 -0400

    userDOmain to web url

--- ui/src/App.js
+++ ui/src/App.js
@@ -23,9 +23,7 @@ export const App = () => {
 
   // const userDomain = "localhost";
   const userDomain =
-  // "vehiclegatekioskui2-env.eba-vgfrxrgp.us-gov-west-1.elasticbeanstalk.com";
-  // "http://vehiclegatekioskui2-env.eba-vgfrxrgp.us-gov-west-1.elasticbeanstalk.com";
-  "http://kioskapi3-env.eba-mxp73pfs.us-gov-west-1.elasticbeanstalk.com/api";
+  "http://kioskapp-env.eba-umdxbzym.us-gov-west-1.elasticbeanstalk.com"
 
   // const API = "http://localhost:8080/api";
   const API =
