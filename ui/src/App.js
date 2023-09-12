594499cfb967862f0a94a0e2ab053b78f59430f1
LordYoungblood <ParissYoungblood@gmail.com>
2023-09-12 10:57:45 -0400

    Add loader using LoadingContext to the history log

--- ui/src/App.js
+++ ui/src/App.js
@@ -11,6 +11,7 @@ import { useCookies } from "react-cookie";
 import { Users } from "./components/admin/admin";
 import { LocalizationProvider } from "@mui/x-date-pickers";
 import { AdapterDayjs } from "@mui/x-date-pickers/AdapterDayjs";
+import { LoadingProvider } from "./components/contexts/LoadingContext";
 
 export const App = () => {
   const [base, setBase] = useState(null);
@@ -19,78 +20,31 @@ export const App = () => {
   const [flag, setFlag] = useState(false);
   const [token, setToken] = useState(null);
   const [user, setUser] = useState(null);
-  const [ visitorDetails, setVisitorDetails] = useState([]);
-  
-  // const userDomain = "localhost";
+  const [visitorDetails, setVisitorDetails] = useState([]);
+
   const userDomain =
-  "http://kioskapp-env.eba-umdxbzym.us-gov-west-1.elasticbeanstalk.com"
-  
- 
-  // const API = "http://localhost:8080/api";
+    "http://kioskapp-env.eba-umdxbzym.us-gov-west-1.elasticbeanstalk.com";
   const API =
     "http://kioskapi3-env.eba-mxp73pfs.us-gov-west-1.elasticbeanstalk.com/api";
 
-    // const [baseName, setBaseName] = useState('');
+  const userRights = localStorage.getItem("user_rights");
+  const userToken = localStorage.getItem("token");
+  const userTokenn = localStorage.getItem("token")
+    ? localStorage.getItem("token").replace(/^"(.*)"$/, "$1")
+    : null;
 
-    // useEffect(() => {
-    //   const storedValue = localStorage.getItem('base')
-    //     const containsPatrick = storedValue.includes('patrick_sfb');
-    //     setBaseName (containsPatrick ? 'Patrick SFB' : 'NOTHING')
-    // }, [])
+  const getBaseName = () => {
+    const baseSetter = { name: "patrick_sfb" };
+    return baseSetter.name;
+  };
 
-    const userRights = localStorage.getItem('user_rights');//sets variable for admin level 
-    const userToken = localStorage.getItem('token');//sets variable for token
+  const baseName = getBaseName();
 
-
-
-
-//write me a function that sets usertokenn if localStorage.getItem('token') is not null
-const userTokenn = localStorage.getItem('token') ? localStorage.getItem('token').replace(/^"(.*)"$/, '$1') : null
-
-    // const userTokenn = localStorage.getItem('token').replace(/^"(.*)"$/, '$1') 
-    
-
-    
-
-
-    //function that maps localStorage.getItem('base') to the name in the object
-    const baseSetter = localStorage.getItem('base')
-    
-    //set base to the name in the baseSetter object
-  
-    const getBaseName = () => {
-      const baseSetter = {"name": "patrick_sfb"};
-      return baseSetter.name;
-    };
-    
-    const baseName = getBaseName();
-    
-  
   useMemo(() => {
-      setBase(baseName);
-      setUser(userRights);
-      setToken(userToken);
-  }, [flag]);
-
-
-
-
-
-
-  // useMemo(() => {
-  //   if (localStorage) {
-  //     const user = JSON.parse(`${localStorage.au}`);
-  //     const token = JSON.parse(`${localStorage.ver}`);
-  //     setUser(user);
-  //     setToken(token);
-  //   }
-  // }, [flag]);
-
-  // console.log("flag from app.js", flag)
-  // console.log("cookies from app.js", cookies)
-  // console.log("token from login", token)
-
-  // ----------------- fetch for all Vehicle information -------------------------//
+    setBase(baseName);
+    setUser(userRights);
+    setToken(userToken);
+  }, [baseName, userRights, userToken]);
 
   useEffect(() => {
     fetch(API, {
@@ -108,11 +62,8 @@ const userTokenn = localStorage.getItem('token') ? localStorage.getItem('token')
         setFlag(false);
       })
       .catch((err) => console.log(err));
-  }, [token, flag]);
+  }, [token, flag, base, userTokenn]);
 
- 
-
-  // ----------- Context object to be passed to all components ------------------//
   const obj = {
     visitorDetails,
     setVisitorDetails,
@@ -130,13 +81,9 @@ const userTokenn = localStorage.getItem('token') ? localStorage.getItem('token')
     removeCookie,
     userDomain,
     setFlag,
-    userRights
+    userRights,
   };
 
-  // console.log('token from app.js', token)
-  // console.log('obj from app.js', obj)
-
-  // ----------------- verification routes for login ----------------------------//
   if (!user) {
     return (
       <VehicleContext.Provider value={obj}>
@@ -153,17 +100,19 @@ const userTokenn = localStorage.getItem('token') ? localStorage.getItem('token')
   return (
     <LocalizationProvider dateAdapter={AdapterDayjs}>
       <VehicleContext.Provider value={obj}>
-        <Router>
-          <Navbar />
-          <Routes>
-            <Route path="/forms" element={<Forms />} />
-            <Route path="/data" element={<Data />} />
-            <Route path="/History" element={<History />} />
-            <Route path="/users" element={<Users />} />
-            <Route path="*" element={<Login />} />
-            <Route path="/webmaster" element={<Webmaster />} />
-          </Routes>
-        </Router>
+        <LoadingProvider>
+          <Router>
+            <Navbar />
+            <Routes>
+              <Route path="/forms" element={<Forms />} />
+              <Route path="/data" element={<Data />} />
+              <Route path="/History" element={<History />} />
+              <Route path="/users" element={<Users />} />
+              <Route path="*" element={<Login />} />
+              <Route path="/webmaster" element={<Webmaster />} />
+            </Routes>
+          </Router>
+        </LoadingProvider>
       </VehicleContext.Provider>
     </LocalizationProvider>
   );
--- ui/src/components/history/history.js
+++ ui/src/components/history/history.js
@@ -1,9 +1,9 @@
 import { useState, useContext, useEffect } from "react";
 import { VehicleContext } from "../VehicleContext";
-import { Box, Typography } from "@mui/material";
+import { useLoading } from "../contexts/LoadingContext";
+import { Box, CircularProgress } from "@mui/material";
 import {
   DataGrid,
-  GridToolbar,
   GridToolbarContainer,
   GridToolbarColumnsButton,
   GridToolbarFilterButton,
@@ -11,41 +11,9 @@ import {
 } from "@mui/x-data-grid";
 
 export const History = () => {
-  const { base, API, token, setFlag, flag } = useContext(VehicleContext);
-  const [baseName, setBaseName] = useState('');
+  const { visitorDetails } = useContext(VehicleContext);
+  const { isLoading, setIsLoading } = useLoading();
   const [pageSize, setPageSize] = useState(50);
-  const userToken = localStorage.getItem('token').replace(/^"(.*)"$/, '$1');
-  const [visitorDetails, setVisitorDetails] = useState([]);
-
-  useEffect(() => {
-    fetch(API, {
-      method: "GET",
-      credentials: "include",
-      headers: {
-        "Content-Type": "application/json; charset=utf-8",
-        Authorization: `Bearer ${userToken}`,
-        Base: JSON.stringify(base),
-      },
-    })
-      .then((res) => res.json())
-      .then((json) => {
-        setVisitorDetails(json);
-        setFlag(false);
-      })
-      .catch((err) => console.log(err));
-  }, []);
-
-  // console.log("visitorDetails from app.js", visitorDetails)
-
-  useEffect(() => {
-    const storedValue = localStorage.getItem('base')
-      const containsPatrick = storedValue.includes('patrick_sfb');
-      setBaseName (containsPatrick ? 'Patrick SFB' : 'NOTHING')
-  }, [])
-
-
-
-
 
   const columns = [
     {
@@ -57,75 +25,56 @@ export const History = () => {
       field: "firstName",
       headerName: "First name",
       width: 150,
-      // editable: true,
     },
     {
       field: "lastName",
       headerName: "Last name",
       width: 150,
-      // editable: true,
     },
     {
       field: "state",
       headerName: "State",
-      // type: 'number',
       width: 110,
-      // editable: true,
     },
     {
       field: "drivers_license",
       headerName: "Drivers License",
       type: "number",
       width: 110,
-      // editable: true,
     },
     {
       field: "plate",
       headerName: "plate",
-      // type: 'number',
       width: 110,
-      // editable: true,
     },
     {
       field: "make",
       headerName: "Make",
-      // type: 'number',
       width: 110,
-      // editable: true,
     },
     {
       field: "model",
       headerName: "Model",
-      // type: 'number',
       width: 110,
-      // editable: true,
     },
     {
-      field: "fullDL",
+      field: "fullName",
       headerName: "Full DL",
+      description: "This column has a value getter and is not sortable.",
       sortable: false,
       width: 160,
       valueGetter: (params) =>
         `${params.row.state || ""} ${params.row.drivers_license || ""}`,
     },
-    {
-      field: "delivery_location",
-      headerName: "Delivery Location",
-      // type: 'number',
-      width: 110,
-      // editable: true,
-    },
   ];
 
-
   const rows = [];
-  visitorDetails?.map((visitor) => {
+  visitorDetails.map((visitor) => {
     rows.push({
       date:
         visitor.date.split("T")[0] +
         " : " +
         visitor.date.split("T")[1].slice(0, 5),
-      // time: visitor.date.split("T")[1].split(".")[0],
       id: visitor.id,
       lastName: visitor.last_name,
       firstName: visitor.first_name,
@@ -134,12 +83,17 @@ export const History = () => {
       plate: visitor.plate,
       make: visitor.make,
       model: visitor.model,
-      delivery_location: visitor.delivery_location,
     });
     return rows;
   });
 
-
+  useEffect(() => {
+    // Simulate a delay before loading the data
+    setIsLoading(true);
+    setTimeout(() => {
+      setIsLoading(false);
+    }, 1000); // 1 seconds delay
+  }, [setIsLoading]);
 
   function CustomToolbar() {
     return (
@@ -153,35 +107,40 @@ export const History = () => {
 
   return (
     <>
-      <Box sx={{ display: "flex", justifyContent: "center", flexGrow: 1 }}>
-      <Typography
-          style={{ alightContent: "center", fontFamily: "sans", fontSize: 30, fontWeight: "bold" }}
+      {isLoading ? (
+        <Box
+          sx={{
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
+            height: "77vh",
+          }}
         >
-          {baseName} History
-          
-        </Typography>
-        {/* <SearchBar /> */}
-      </Box>
-      <Box>
-        {/* <Box sx={{ height: 700, width: "100%" }}> */}
-        <div style={{ height: "77vh", width: "100%" }}>
-          <DataGrid
-            rows={rows}
-            columns={columns}
-            pageSize={pageSize}
-            onPageSizeChange={(e) => setPageSize(e)}
-            rowsPerPageOptions={[10, 20, 50, 100]}
-            // checkboxSelection
-            disableSelectionOnClick
-            experimentalFeatures={{ newEditingApi: true }}
-            sx={{ color: 'black', '& .MuiButtonBase-root': {color: 'black'}}}
-            
-            components={{
-              Toolbar: CustomToolbar,
-            }}
-          ></DataGrid>
-        </div>
-      </Box>
+          <CircularProgress />
+        </Box>
+      ) : (
+        <>
+          <Box sx={{ display: "flex", justifyContent: "center", flexGrow: 1 }}>
+            <h1>History</h1>
+          </Box>
+          <Box>
+            <div style={{ height: "77vh", width: "100%" }}>
+              <DataGrid
+                rows={rows}
+                columns={columns}
+                pageSize={pageSize}
+                onPageSizeChange={(e) => setPageSize(e)}
+                rowsPerPageOptions={[10, 20, 50, 100]}
+                disableSelectionOnClick
+                experimentalFeatures={{ newEditingApi: true }}
+                components={{
+                  Toolbar: CustomToolbar,
+                }}
+              ></DataGrid>
+            </div>
+          </Box>
+        </>
+      )}
     </>
   );
 };
